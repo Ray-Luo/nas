c8b3a31acfe56be8d996e327be6ebfd01335c592
leiluoray <leiluo.ray@gmail.com>
2023-04-05 18:15:44 -0700

    more

--- measure_latency.py
+++ measure_latency.py
@@ -26,28 +26,35 @@ def measure_latency(operation, input_tensor, num_runs=100):
 """
 measurement configs
 """
-FEATURE_DEPTH = 1024 + 1
-FEATURE_WIDTH_MAX = 512 + 1
-FEATURE_WIDTH_MIN = 3
+DEPTH_MAX = 150 + 1
+DEPTH_MIN = 1
+DEPTH_INTERVAL = 5
+
+FEATURE_DIM_MAX = 32 + 1
+FEATURE_DIM_MIN = 4
+FEATURE_INTERVAL = 1
+
+FEATURE_DEPTH_LIST = range(DEPTH_MIN, DEPTH_MAX, DEPTH_INTERVAL)
+FEATURE_DIM_LIST = range(FEATURE_DIM_MIN, FEATURE_DIM_MAX, 1)
+
 
 """
 Measure Channel Masking
 """
-MAX_CHANNEL = 1024 + 1
+MAX_CHANNEL = 150 + 1
 MAX_NUM_MASK = 5 + 1
 
 if 1:
     operation_configs = []
-    for in_channels in range(1, FEATURE_DEPTH):
-        for max_channels_i in range(0, MAX_CHANNEL):
-            max_out_channels = max_channels_i + in_channels
-            if max_out_channels > MAX_CHANNEL:
+    for in_channels in FEATURE_DEPTH_LIST:
+        for max_channels in range(in_channels, MAX_CHANNEL, DEPTH_INTERVAL):
+            if max_channels > MAX_CHANNEL:
                 break
-            for num_masks in range(1,MAX_NUM_MASK):
-                operation_configs.append({'type': 'channel_mask', 'in_channels': in_channels, 'max_out_channels': max_out_channels, 'num_masks': num_masks})
+            for num_masks in range(1, MAX_NUM_MASK):
+                operation_configs.append({'type': 'channel_mask', 'in_channels': in_channels, 'max_out_channels': max_channels, 'num_masks': num_masks})
 
     input_configs = []
-    for width in range(FEATURE_WIDTH_MIN, FEATURE_WIDTH_MAX):
+    for width in FEATURE_DIM_LIST:
         input_configs.append({'type': 'tensor', 'batch_size': 1, 'height': width, 'width': width})
 
     latency_lut = {}
@@ -58,7 +65,7 @@ if 1:
     latency_mean = []
 
     for input_config in tqdm(input_configs):
-        for operation_config in operation_configs:
+        for operation_config in tqdm(operation_configs):
             input_tensor = torch.rand(input_config["batch_size"], operation_config['in_channels'], input_config["height"], input_config["width"])
             if operation_config['type'] == 'channel_mask':
                 op = ChannelMask(operation_config['in_channels'], operation_config['max_out_channels'], operation_config['num_masks'])
